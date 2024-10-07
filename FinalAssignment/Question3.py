c2256d44e345e3b4aa5127eebed948b8f7e219e3
mohsinandsarker <mohsinsarker33@gmail.com>
2024-10-07 23:54:17 +0600

    Add files via upload

---
+++ FinalAssignment/Question3.py
@@ -0,0 +1,6 @@
+s = input("Enter a string: ")
+def count_digits(s):
+    count = sum(c.isdigit() for c in s)
+    return count
+
+print(f"Total digits are: {count_digits(s)}")
---
+++ FinalAssignment/question4.py
@@ -0,0 +1,5 @@
+s = input("Enter a string: ")
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(is_palindrome(s))
